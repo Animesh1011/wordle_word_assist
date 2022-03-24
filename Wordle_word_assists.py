9c33692a2714a80b04e4ff060dfc0c01a475174b
Animesh Baidya <62639113+Animesh1011@users.noreply.github.com>
2022-03-24 23:32:00 +0530

    Add files via upload

--- Wordle_word_assists.py
+++ Wordle_word_assists.py
@@ -1,57 +1,46 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
-
-
-#!pip install english-words
-
-
-
-from english_words import english_words_lower_alpha_set
-
-
-
-
-
 #wordle_word_assists
 
-def wordle_word_assists(startswith='',second_position='',middle_position='',fourth_position='',endswith='',include='',doesnotinclude='',contain_not_in_pos=''):
-    #variable decralation
-    startswith = startswith
-    second_position = second_position
-    middle_position = middle_position
-    fourth_position = fourth_position
-    endswith = endswith
-    include = include
-    doesnotinclude = doesnotinclude
-    contain_not_in_pos = contain_not_in_pos
+with open(r'C:\Users\USER\Downloads\words_alpha.txt') as word_file:
+    valid_words = set(word_file.read().split())
+
+def wordle_word_assists(startswith='',second_position='',middle_position='',fourth_position='',endswith='',doesnotinclude='',contain_not_in_pos='',repetitive_letter='',length_of_word=5):
     
-    five_word_list = [x for x in english_words_lower_alpha_set if len(x) == 5]
+    five_word_list = [x for x in valid_words if len(x) == length_of_word]
     final_word_list = five_word_list
     
     #doesnotinclude
+    
+    #check if any letter present in other filter.
+    letters_without_number = ''.join(filter(lambda x : not x.isdigit(),contain_not_in_pos))
+    total_input_letters = set(startswith+second_position+middle_position+fourth_position+endswith+letters_without_number)
+    
+    for letter in total_input_letters:
+        if letter in doesnotinclude:
+            doesnotinclude = doesnotinclude.replace(letter,'')
     doesnotinclude_list = [x for x in doesnotinclude]
+    
     if doesnotinclude_list:
         for letter in doesnotinclude_list:
             for word in final_word_list[:]:
                 if letter in word:
                     final_word_list.remove(word)
-    
-    #include
-    include_list = [x for x in include]
-    if include_list:
-        for letter in include_list:
-            for word in final_word_list[:]:
-                if letter not in word:
-                    final_word_list.remove(word)
                     
     #contains_but_not_in_nth_position
     contain_not_in_pos_list = [(contain_not_in_pos[x],contain_not_in_pos[x+1]) for x in range(0,len(contain_not_in_pos),2)]
     if contain_not_in_pos_list:
+        include_list = []
         for tuples in contain_not_in_pos_list:
             for word in final_word_list[:]:
                 if word[int(tuples[0])-1]==tuples[1]:
+                    final_word_list.remove(word)
+                    if tuples[1] not in include_list:
+                        include_list.append(tuples[1])
+                    
+        for letter in include_list:
+            for word in final_word_list[:]:
+                if letter not in word:
                     final_word_list.remove(word)
                     
     #startswith
@@ -83,10 +72,17 @@ def wordle_word_assists(startswith='',second_position='',middle_position='',four
         for word in final_word_list[:]:
             if word[4]!=endswith:
                 final_word_list.remove(word)
+                
+    repetitive_list = [(repetitive_letter[x],repetitive_letter[x+1]) for x in range(0,len(repetitive_letter),2)]
+    if repetitive_list:
+        for tuples in repetitive_list:
+            for word in final_word_list[:]:
+                if word.count(tuples[1])<int(tuples[0]):
+                    final_word_list.remove(word)
     return final_word_list
 
 
-print(wordle_word_assists(contain_not_in_pos='',startswith='',second_position='',middle_position='',fourth_position='',endswith='',include='',doesnotinclude=''))
+print(wordle_word_assists(length_of_word = 5,contain_not_in_pos='',repetitive_letter='',startswith='',second_position='',middle_position='',fourth_position='',endswith='',doesnotinclude=''))
 
 
 
